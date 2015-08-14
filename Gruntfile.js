a81f51bfda6af1caf47f6c9f06371271e205767f
Dany Shaanan <danyshaanan@gmail.com>
2015-08-14 17:28:54 +0300

    Expand eslint files pattern

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     eslint: {
-      target: ['lib/**/*.js', 'bin/**/*.js']
+      target: ['**/*.js', '!node_modules/**/*']
     },
     bump: {
       options: {
