da6d353d1be68269d6e5912214a381a7c74944d6
Dany Shaanan <danyshaanan@gmail.com>
2016-05-02 00:16:18 +0300

    Refactor colorByIterations

--- src/main/lib/colorByIterations.js
+++ src/main/lib/colorByIterations.js
@@ -2,9 +2,6 @@
 
 const clc = require('cli-color')
 
-const clcColors = [196, 202, 208, 214, 220, 226, 190, 154, 118, 82, 46, 47, 48, 49, 50, 51, 45, 39, 33, 27, 21, 57, 93, 129, 165, 201]
+const colors = [196, 202, 208, 214, 220, 226, 190, 154, 118, 82, 46, 47, 48, 49, 50, 51, 45, 39, 33, 27, 21, 57, 93, 129, 165, 201]
 
-module.exports = iterations => {
-  const color = iterations === -1 ? 0 : clcColors[iterations % clcColors.length]
-  return clc.bgXterm(color)(' ')
-}
+module.exports = i => clc.bgXterm(i === -1 ? 0 : colors[i % colors.length])(' ')
