da0483b527c3d66e3a2c0bf5fb35ac25f06b52f4
Dany Shaanan <danyshaanan@gmail.com>
2016-05-01 23:30:44 +0300

    Refactor iterationsToEscape

--- src/main/lib/math.js
+++ src/main/lib/math.js
@@ -1,20 +1,12 @@
-'use strict'
+'use strict' /* eslint curly: [2,"multi"] */
 
 function iterationsToEscape(x0, y0, iterations) {
-  let x = x0
-  let y = y0
-  let i = 0
-
-  function iterate() {
-    i++
-    [x, y] = [x * x - y * y + x0, 2 * x * y + y0]
+  let [x, y, i] = [x0, y0, 0]
+  while (true) {
+    if (x * x + y * y >= 4) return i
+    ;[x, y, i] = [x * x - y * y + x0, 2 * x * y + y0, i + 1]
+    if (i > iterations) return -1
   }
-
-  while (x * x + y * y < 4) {
-    if (i === iterations) return -1
-    iterate()
-  }
-  return i
 }
 
 module.exports = { iterationsToEscape }
