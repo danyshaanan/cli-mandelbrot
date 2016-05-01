dabc2700541698ab5a15cf2d8627c877d0d38f57
Dany Shaanan <danyshaanan@gmail.com>
2016-05-02 00:43:14 +0300

    Refactor calculateScreen

--- src/main/lib/calculateScreen.js
+++ src/main/lib/calculateScreen.js
@@ -3,23 +3,10 @@
 const iterationsToEscape = require('./iterationsToEscape.js')
 const colorByIterations = require('./colorByIterations.js')
 
-const emptyArray = l => Array.from(Array(l))
-
-const getXs = (def, width) =>
-  emptyArray(width).map(
-    (v, w) => (w - width / 2) / def.pixelsPerUnit + def.x
-  )
-
-const getYs = (def, height) =>
-  emptyArray(height).map(
-    (v, h) => (h - height / 2) / def.pixelsPerUnit * def.pixelHightByWidthRatio + def.y
-  )
+const getColor = (x, y, iterations) => colorByIterations(iterationsToEscape(x, y, iterations))
 
 module.exports = (def, w, h) => {
-  const xs = getXs(def, w)
-  const ys = getYs(def, h)
-  return ys.map(y =>
-    xs.map(
-      x => colorByIterations(iterationsToEscape(x, y, def.iterations))).join('')
-  ).join('\n')
+  const xs = Array.from(Array(w), (_, i) => def.x + (i - w / 2) / def.pixelsPerUnit)
+  const ys = Array.from(Array(h), (_, i) => def.y + (i - h / 2) / def.pixelsPerUnit * def.pixelHightByWidthRatio)
+  return ys.map(y => xs.map(x => getColor(x, y, def.iterations)).join('')).join('\n')
 }
