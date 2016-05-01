daf157296417ca82fa85fbf7c5efe72f009adc31
Dany Shaanan <danyshaanan@gmail.com>
2016-05-01 21:48:37 +0300

    Add test for calculateScreen

---
+++ src/test/lib/grid.spec.js
@@ -0,0 +1,20 @@
+'use strict'
+
+import test from 'ava'
+import { calculateScreen } from '../../main/lib/grid.js'
+
+const def = {
+  pixelsPerUnit: 32,
+  pixelHightByWidthRatio: 3,
+  x: -1.149719506110225,
+  y: -0.312197910519423,
+  iterations: 100,
+  ascii: false
+}
+
+const result = calculateScreen(def, 2, 2)
+const expected = '\u001b[48;5;190m \u001b[49m\u001b[48;5;190m \u001b[49m\n\u001b[48;5;39m \u001b[49m\u001b[48;5;0m \u001b[49m'
+
+test('keyPress should do nothing for undefined key', t => {
+  t.same(result, expected, '...')
+})
