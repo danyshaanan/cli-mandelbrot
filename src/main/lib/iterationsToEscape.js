da145139b797b1f92a4b4d94977960f99abf8b27
Dany Shaanan <danyshaanan@gmail.com>
2016-05-02 04:01:26 +0300

    Fix performance regression by removing destructuring

--- src/main/lib/iterationsToEscape.js
+++ src/main/lib/iterationsToEscape.js
@@ -1,10 +1,15 @@
-'use strict' /* eslint curly: [2,"multi"] */
+'use strict'
+
+let t
 
 module.exports = (x0, y0, iterations) => {
   let [x, y, i] = [x0, y0, 0]
   while (true) {
     if (x * x + y * y >= 4) return i
-    ;[x, y, i] = [x * x - y * y + x0, 2 * x * y + y0, i + 1]
+    t = x * x - y * y + x0
+    y = 2 * x * y + y0
+    x = t
+    i++
     if (i > iterations) return undefined
   }
 }
